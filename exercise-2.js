c8f4a2af2517398e7d071e4f7ec63aeff181c0ac
Aviva Wolpert <wolpertaviva@gmail.com>
2015-09-24 20:45:20 -0400

    Update exercise-2.js
    Completed exercise

--- exercise-2.js
+++ exercise-2.js
@@ -27,4 +27,61 @@ delay(500).then(function(){
    
 }).then (console.log);
 
+
+
+function getLastChar(string){
+    
+        return new Promise(function(resolve, reject) {
+            resolve(string.charAt(string.length - 1))
+            
+    });
+}
+
+delay(500).then(function(){
+    
+    
+    return getLastChar("Alphabet");
+    
+   
+}).then (console.log);
+
+//
+
+function getFirstAndLastCharSeq(string) {
+    var firstChar;
+   return getFirstChar(string).then(
+        function(_firstChar) {
+            firstChar = _firstChar;
+            return getLastChar(string);
+        }
+    ).then(
+        function(lastChar) {
+            return firstChar + lastChar
+        }
+        );
+}
+
+getFirstAndLastCharSeq("Hello").then (
+    function(firstLast) {
+        console.log(firstLast);
+    }
+    );
+    
+//
+
+function getFirstAndLastCharParallel(string) {
+    var firstCharPromise = getFirstChar(string);
+    var lastCharPromise = getLastChar(string);
+    return Promise.join(firstCharPromise, lastCharPromise, function(firstChar, lastChar) {
+        return firstChar + lastChar;
+        
+    });
+}
+
+getFirstAndLastCharParallel("Hello").then(
+    function(firstLast) {
+        console.log(firstLast);
+    }
+    );
+
  
